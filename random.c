6ce4dd2443258926ea668ea6b8e73ca679ede171
Reda Kadri <rkadri@c1r5p14.1337.ma>
2026-08-01 06:52:53 +0100

    random

--- random.c
+++ random.c
@@ -1,53 +1,24 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   random.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rkadri <rkadri@student.1337.ma>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/07/26 02:36:24 by rkadri            #+#    #+#             */
-/*   Updated: 2026/07/26 03:46:25 by rkadri           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <pthread.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
-int				MAILS = 0;
-pthread_mutex_t	mutexi = PTHREAD_MUTEX_INITIALIZER;
+int		n = 100;
 
-void	*routing(void *arg)
+void	*worker(void *arg)
 {
-	// printf("thread %d is saying hi...\n", *(int *)arg);
-	// sleep(2);
-	for (int i = 0; i < 10000000; i++)
-	{
-		pthread_mutex_lock(&mutexi);
-		MAILS += 1;
-		pthread_mutex_unlock(&mutexi);
-	}
+	printf("thread %ld\n", *(long *)arg);
 	return (NULL);
 }
 
-int	main(int argc, char *argv[])
+int	main(void)
 {
-	pthread_t	t[5];
-	int			*a;
-	int			r;
+	pthread_t	t;
+	long		i;
 
-	r = 1;
-	for (int i = 0; i < 5; i++)
+	i = 0;
+	while (1)
 	{
-		a = malloc(sizeof(int));
-		*a = i;
-		pthread_create(&t[i], NULL, routing, a);
+		pthread_create(&t, NULL, worker, &i);
+		i++;
 	}
-	for (int i = 0; i < 5; i++)
-	{
-		pthread_join(t[i], NULL);
-	}
-	printf("new mails: %d\n\n", MAILS);
 	return (0);
 }
